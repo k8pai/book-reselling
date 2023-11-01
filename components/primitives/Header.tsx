8728c3ddde442b22db8315ab050f62423e5cfcd4
k8pai <thek8pai@gmail.com>
2023-11-01 09:21:29 +0530

    mend

--- components/primitives/Header.tsx
+++ components/primitives/Header.tsx
@@ -25,13 +25,9 @@ import {
 import { Skeleton } from '../ui/Skeleton';
 
 export function Header() {
-	const [openNav, setOpenNav] = useState(false);
 	const { data: session, status } = useSession();
 
-	console.log(`status = ${status}, session => ${session}`);
-	// if (status === 'loading') {
-	// 	return <>we're loading bitch</>;
-	// }
+	// console.log(`status = ${status}, session => ${session}`);
 
 	const NavLinks = useCallback(() => {
 		const [openMenu, setOpenMenu] = useState(false);
@@ -179,13 +175,6 @@ export function Header() {
 		}
 	}, [session, status]);
 
-	// useEffect(() => {
-	// 	window.addEventListener(
-	// 		'resize',
-	// 		() => window.innerWidth >= 960 && setOpenNav(false),
-	// 	);
-	// }, []);
-
 	return (
 		<Navbar className="mx-auto max-w-full px-4 py-2 rounded-lg lg:px-8 lg:py-4">
 			<div className="mx-auto flex items-center justify-between text-blue-gray-900">
@@ -253,69 +242,7 @@ export function Header() {
 					</Typography>
 					<NavLinks />
 				</div>
-
-				{/* <IconButton
-					variant="text"
-					className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
-					ripple={false}
-					onClick={() => setOpenNav(!openNav)}
-				>
-					{openNav ? (
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							className="h-6 w-6"
-							viewBox="0 0 24 24"
-							stroke="currentColor"
-							strokeWidth={2}
-						>
-							<path
-								strokeLinecap="round"
-								strokeLinejoin="round"
-								d="M6 18L18 6M6 6l12 12"
-							/>
-						</svg>
-					) : (
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							className="h-6 w-6"
-							fill="none"
-							stroke="currentColor"
-							strokeWidth={2}
-						>
-							<path
-								strokeLinecap="round"
-								strokeLinejoin="round"
-								d="M4 6h16M4 12h16M4 18h16"
-							/>
-						</svg>
-					)}
-				</IconButton> */}
 			</div>
-			{/* <MobileNav open={openNav}>
-				<div className="container mx-auto">
-					<div className="flex items-center gap-x-1">
-						<Button
-							fullWidth
-							variant="text"
-							size="sm"
-							onClick={() => signIn()}
-							className=""
-						>
-							<span>Log In</span>
-						</Button>
-						<Button
-							fullWidth
-							variant="gradient"
-							size="sm"
-							className=""
-							onClick={() => redirect('/authenticate/signup')}
-						>
-							<span>Sign up</span>
-						</Button>
-					</div>
-				</div>
-			</MobileNav> */}
 		</Navbar>
 	);
 }
