9465997efdb460441f27fa560a0ed856f38de860
k8pai <thek8pai@gmail.com>
2023-11-01 09:32:37 +0530

    -ammend

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,19 @@ import { Books } from '@prisma/client';
 
 export const dynamic = 'force-dynamic';
 
-export default async function Home() {
+const fetchData = async (): Promise<Books[]> => {
+	'server only';
 	const response = await fetch(`${process.env.NEXTAUTH_URL}/api/books`, {
 		next: { revalidate: 1 },
 	});
 	const data = await response.json();
 
+	return { ...data };
+};
+
+export default async function Home() {
 	// console.log('data from home => ', data);
-	const contents: Books[] = data.data;
+	const contents = await fetchData();
 
 	return (
 		<section className=" container mx-auto py-6">
